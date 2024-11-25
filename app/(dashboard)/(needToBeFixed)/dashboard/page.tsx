8bc02c375a9af2bc773b2b30183fa6601fff0903
Jingwu Wang <jingwuwang.2001@gmail.com>
2024-11-25 10:44:48 -0600

    format dashboard

--- app/(dashboard)/(needToBeFixed)/dashboard/page.tsx
+++ app/(dashboard)/(needToBeFixed)/dashboard/page.tsx
@@ -8,12 +8,12 @@ import DashboardCardVehicle from  './dashboard-card-vehicle'
 
 export default function Dashboard() {
   return (
-    <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-[96rem] mx-auto">
+    <div className="px-8 py-8 w-full max-w-[96rem] mx-auto">
      
-      <div className="sm:flex sm:justify-between sm:items-center mb-8">
+      <div className="flex justify-between items-center mb-8">
         {/* Left: Title */}
-        <div className="mb-4 sm:mb-0">
-          <h1 className="text-2xl md:text-3xl text-gray-800 dark:text-gray-100 font-bold">Dashboard</h1>
+        <div className="mb-0">
+          <h1 className="text-3xl text-gray-100 font-bold">Dashboard</h1>
         </div>
       </div>
 
