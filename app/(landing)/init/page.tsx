0eb0e7c91096835b20b2e19aac6c31199ea95d70
Jingwu Wang <jingwuwang.2001@gmail.com>
2024-11-25 00:44:12 -0600

    fixed style of init page

--- app/(landing)/init/page.tsx
+++ app/(landing)/init/page.tsx
@@ -9,7 +9,7 @@ export default function SignUp() {
     simulationTime: "",
     // accelerationFactor: "",
     timeStep: "",
-    electricityRate: "",
+    // electricityRate: "",
     electricityPrice: "",
     robotaxiCount: "",
     chargingStationCount: "",
@@ -68,58 +68,52 @@ export default function SignUp() {
   };
 
   return (
-    <section className="relative">
-      <div className="px-4">
-        <div className="pt-20 pb-20">
-          <div className="text-center pb-10">
-            <h1 className="h1">
-              Welcome. 
-            </h1>
-            <h1 className="h1">
-            We exist to make robotaxi charging easier.
-            </h1>
-          </div>
+    <section className="relative px-4">
+      <div className="pt-20 pb-10">
+        <div className="text-center pb-10">
+          <h1 className="h1">Welcome.</h1>
+          <h1 className="h1">We exist to make robotaxi charging easier.</h1>
+        </div>
 
-          <div className="max-w-sm mx-auto">
-            <form onSubmit={handleSubmit}>
-              <div className="mb-5">
-                <label
-                  className="block text-gray-300 text-sm font-medium mb-1"
-                  htmlFor="city"
-                >
-                  City{" "}
-                  <span className="text-red-600">*</span>
-                </label>
-                <input
-                  id="city"
-                  type="text"
-                  className="form-input w-full text-gray-300"
-                  placeholder="Currently we only support Houston"
-                  required
-                />
-              </div>
+        <div className="max-w-sm mx-auto">
+          <form onSubmit={handleSubmit}>
+            <div className="mb-5">
+              <label
+                className="block text-gray-300 text-sm font-medium mb-1"
+                htmlFor="city"
+              >
+                City <span className="text-red-600">*</span>
+              </label>
+              <input
+                id="city"
+                type="text"
+                className="form-input w-full text-gray-300"
+                placeholder="Currently we only support Houston"
+                required
+              />
+            </div>
 
-              <div className="mb-5">
-                <label
-                  className="block text-gray-300 text-sm font-medium mb-1"
-                  htmlFor="simulationTime"
-                >
-                  Simulation Time (seconds){" "}
-                  <span className="text-red-600">*</span>
-                </label>
-                <input
-                  id="simulationTime"
-                  name="simulationTime"
-                  type="text"
-                  className="form-input w-full text-gray-300"
-                  placeholder="1000"
-                  value={formData.simulationTime}
-                  onChange={handleChange}
-                  required
-                />
-              </div>
+            <div className="mb-5">
+              <label
+                className="block text-gray-300 text-sm font-medium mb-1"
+                htmlFor="simulationTime"
+              >
+                Simulation Time (seconds){" "}
+                <span className="text-red-600">*</span>
+              </label>
+              <input
+                id="simulationTime"
+                name="simulationTime"
+                type="text"
+                className="form-input w-full text-gray-300"
+                placeholder="1000"
+                value={formData.simulationTime}
+                onChange={handleChange}
+                required
+              />
+            </div>
 
-              {/* <div className="mb-5">
+            {/* <div className="mb-5">
                 <label
                   className="block text-gray-300 text-sm font-medium mb-1"
                   htmlFor="accelerationFactor"
@@ -139,139 +133,135 @@ export default function SignUp() {
                 />
               </div> */}
 
-              <div className="mb-5">
-                <label
-                  className="block text-gray-300 text-sm font-medium mb-1"
-                  htmlFor="timeStep"
-                >
-                  Time Step (seconds){" "}
-                  <span className="text-red-600">*</span>
-                </label>
-                <input
-                  id="timeStep"
-                  name="timeStep"
-                  type="text"
-                  className="form-input w-full text-gray-300"
-                  placeholder="1"
-                  value={formData.timeStep}
-                  onChange={handleChange}
-                  required
-                />
-              </div>
-
-              <div className="mb-5">
-                <label
-                  className="block text-gray-300 text-sm font-medium mb-1"
-                  htmlFor="password"
-                >
-                  Electricity Rate ($/kWh){" "}
-                  <span className="text-red-600">*</span>
-                </label>
-                <input
-                  id="password"
-                  type="text"
-                  className="form-input w-full text-gray-300"
-                  placeholder="0.14"
-                  required
-                />
-              </div>
-
-              <div className="mb-5">
-                <label
-                  className="block text-gray-300 text-sm font-medium mb-1"
-                  htmlFor="robotaxiCount"
-                >
-                  Number of Robotaxis <span className="text-red-600">*</span>
-                </label>
-                <input
-                  id="robotaxiCount"
-                  name="robotaxiCount"
-                  type="text"
-                  className="form-input w-full text-gray-300"
-                  placeholder="10"
-                  value={formData.robotaxiCount}
-                  onChange={handleChange}
-                  required
-                />
-              </div>
-
-              <div className="mb-5">
-                <label
-                  className="block text-gray-300 text-sm font-medium mb-1"
-                  htmlFor="chargingStationCount"
-                >
-                  Number of Charging Stations{" "}
-                  <span className="text-red-600">*</span>
-                </label>
-                <input
-                  id="chargingStationCount"
-                  name="chargingStationCount"
-                  type="text"
-                  className="form-input w-full text-gray-300"
-                  placeholder="10"
-                  value={formData.chargingStationCount}
-                  onChange={handleChange}
-                  required
-                />
-              </div>
-
-              <div className="mb-5">
-                <label
-                  className="block text-gray-300 text-sm font-medium mb-1"
-                  htmlFor="peopleCount"
-                >
-                  Number of Passengers{" "}
-                  <span className="text-red-600">*</span>
-                </label>
-                <input
-                  id="peopleCount"
-                  name="peopleCount"
-                  type="text"
-                  className="form-input w-full text-gray-300"
-                  placeholder="10"
-                  value={formData.peopleCount}
-                  onChange={handleChange}
-                  required
-                />
-              </div>
-
-              {error && (
-                <div className="mb-5">
-                  <div className="text-red-600">{error}</div>
-                </div>
-              )}
-
-              <div className="mt-5">
-                <button
-                  type="submit"
-                  className="btn text-white bg-violet-600 hover:bg-violet-700 w-full"
-                >
-                  Start
-                </button>
-              </div>
-            </form>
-          </div>
-
-          <footer className="mt-10 text-center">
-              <p className="text-gray-500 mb-2">
-                Developed by Andrew Negrut, Tarushi Mittal, Jingwu Wang
-              </p>
-              <a
-                className="text-indigo-600 hover:text-indigo-800"
-                href="https://github.com/Jingwu-01/Robotaxi-Front-end"
-                aria-label="Github"
+            <div className="mb-5">
+              <label
+                className="block text-gray-300 text-sm font-medium mb-1"
+                htmlFor="timeStep"
               >
-                <svg
-                  className="h-8 w-8 inline-block"
-                  viewBox="0 0 32 32"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                >
-                  <path d="M16 8.2c-4.4 0-8 3.6-8 8 0 3.5 2.3 6.5 5.5 7.6.4.1.5-.2.5-.4V22c-2.2.5-2.7-1-2.7-1-.4-.9-.9-1.2-.9-1.2-.7-.5.1-.5.1-.5.8.1 1.2.8 1.2.8.7 1.3 1.9.9 2.3.7.1-.5.3-.9.5-1.1-1.8-.2-3.6-.9-3.6-4 0-.9.3-1.6.8-2.1-.1-.2-.4-1 .1-2.1 0 0 .7-.2 2.2.8.6-.2 1.3-.3 2-.3s1.4.1 2 .3c1.5-1 2.2-.8 2.2-.8.4 1.1.2 1.9.1 2.1.5.6.8 1.3.8 2.1 0 3.1-1.9 3.7-3.7 3.9.3.4.6.9.6 1.6v2.2c0 .2.1.5.6.4 3.2-1.1 5.5-4.1 5.5-7.6-.1-4.4-3.7-8-8.1-8z" />
-                </svg>
-              </a>
-            </footer>
+                Time Step (seconds) <span className="text-red-600">*</span>
+              </label>
+              <input
+                id="timeStep"
+                name="timeStep"
+                type="text"
+                className="form-input w-full text-gray-300"
+                placeholder="1"
+                value={formData.timeStep}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div className="mb-5">
+              <label
+                className="block text-gray-300 text-sm font-medium mb-1"
+                htmlFor="password"
+              >
+                Electricity Rate ($/kWh) <span className="text-red-600">*</span>
+              </label>
+              <input
+                id="password"
+                type="text"
+                className="form-input w-full text-gray-300"
+                placeholder="0.14"
+                required
+              />
+            </div>
+
+            <div className="mb-5">
+              <label
+                className="block text-gray-300 text-sm font-medium mb-1"
+                htmlFor="robotaxiCount"
+              >
+                Number of Robotaxis <span className="text-red-600">*</span>
+              </label>
+              <input
+                id="robotaxiCount"
+                name="robotaxiCount"
+                type="text"
+                className="form-input w-full text-gray-300"
+                placeholder="10"
+                value={formData.robotaxiCount}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div className="mb-5">
+              <label
+                className="block text-gray-300 text-sm font-medium mb-1"
+                htmlFor="chargingStationCount"
+              >
+                Number of Charging Stations{" "}
+                <span className="text-red-600">*</span>
+              </label>
+              <input
+                id="chargingStationCount"
+                name="chargingStationCount"
+                type="text"
+                className="form-input w-full text-gray-300"
+                placeholder="10"
+                value={formData.chargingStationCount}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div className="mb-5">
+              <label
+                className="block text-gray-300 text-sm font-medium mb-1"
+                htmlFor="peopleCount"
+              >
+                Number of Passengers <span className="text-red-600">*</span>
+              </label>
+              <input
+                id="peopleCount"
+                name="peopleCount"
+                type="text"
+                className="form-input w-full text-gray-300"
+                placeholder="10"
+                value={formData.peopleCount}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            {error && (
+              <div className="mb-5">
+                <div className="text-red-600">{error}</div>
+              </div>
+            )}
+
+            <div className="mt-5">
+              <button
+                type="submit"
+                className="btn text-white bg-violet-600 hover:bg-violet-700 w-full"
+              >
+                Start
+              </button>
+            </div>
+          </form>
         </div>
+
+        <footer className="mt-10 text-center">
+          <p className="text-gray-500 mb-2">
+            Developed by Andrew Negrut, Tarushi Mittal, Jingwu Wang
+          </p>
+          <a
+            className="text-indigo-600 hover:text-indigo-800"
+            href="https://github.com/Jingwu-01/Robotaxi-Front-end"
+            aria-label="Github"
+          >
+            <svg
+              className="h-8 w-8 inline-block"
+              viewBox="0 0 32 32"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+            >
+              <path d="M16 8.2c-4.4 0-8 3.6-8 8 0 3.5 2.3 6.5 5.5 7.6.4.1.5-.2.5-.4V22c-2.2.5-2.7-1-2.7-1-.4-.9-.9-1.2-.9-1.2-.7-.5.1-.5.1-.5.8.1 1.2.8 1.2.8.7 1.3 1.9.9 2.3.7.1-.5.3-.9.5-1.1-1.8-.2-3.6-.9-3.6-4 0-.9.3-1.6.8-2.1-.1-.2-.4-1 .1-2.1 0 0 .7-.2 2.2.8.6-.2 1.3-.3 2-.3s1.4.1 2 .3c1.5-1 2.2-.8 2.2-.8.4 1.1.2 1.9.1 2.1.5.6.8 1.3.8 2.1 0 3.1-1.9 3.7-3.7 3.9.3.4.6.9.6 1.6v2.2c0 .2.1.5.6.4 3.2-1.1 5.5-4.1 5.5-7.6-.1-4.4-3.7-8-8.1-8z" />
+            </svg>
+          </a>
+        </footer>
       </div>
     </section>
   );
